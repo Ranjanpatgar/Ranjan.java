1452f4c0371d8c3dfc3b5ff9fb0e87133ffb1b36
Ranjanpatgar <157378832+Ranjanpatgar@users.noreply.github.com>
2025-12-05 20:27:36 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+package placement1;
+
+public class Sum {
+
+   public static void main(String[] args) {
+		        int[] arr = {10, 20, 30, 40, 50};  // sample array
+		        int sum = 0;
+
+		        for(int i = 0; i < arr.length; i++) {
+		            sum += arr[i];
+		        }
+
+		        System.out.println("Sum of array elements = " + sum);
+		    }
+		
+	}
+
+
+
